7754de030c0b3f0b0a685a915d2fb7d198c54a7a
IvanGallegosAragon <89938838+IvanGallegosAragon@users.noreply.github.com>
2021-09-09 23:15:19 -0500

    Add files via upload

---
+++ Tarea3 ADT Array/Array.py
@@ -0,0 +1,34 @@
+"""ADT Array
+    ADT lineal estatico
+"""
+
+class Array():
+  def __init__( self , tamano): #Constructor
+    self.__tamano = tamano
+    self.__data = [0 for x in range(self.__tamano)] #Generator declara una lista de 0 usando un for 
+
+  def getItem(self, index):
+    return self.__data[index]
+  
+  def toString(self):
+    print(self.__data)
+  
+  def setItem(self, dato, index):
+    self.__data[index] = dato   #Solo asignar por indice 
+  
+  def getLength(self):
+      return self.__tamano #Ya tenemos un atributo que retornar sin usar __len__()
+  
+  def clear(self, dato):
+    self.__data =  [dato for x in self.__data]  #Un nuevo generator que recorre el array
+
+
+#Ejemplo
+ejemplo = Array(5)
+ejemplo.toString()
+ejemplo.getItem(3)
+ejemplo.setItem(4,2)
+ejemplo.toString()
+print(ejemplo.getLength())
+ejemplo.clear(7)
+ejemplo.toString()
