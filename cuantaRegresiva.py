cd2568a313b1ec08a1fc7678685a7a0229ce5d3d
IvanGallegosAragon <89938838+IvanGallegosAragon@users.noreply.github.com>
2021-10-06 13:42:07 -0500

    Delete cuantaRegresiva.py

--- cuantaRegresiva.py
+++
@@ -1,8 +0,0 @@
-def cuentaRegresiva(n): #Es basicamente una modificacion del código visto en clase 
-    if n >= 1:
-        print(n) 
-        cuentaRegresiva(n-1)
-    else: 
-        print("BOOMMMMM !!!")
-n = 3
-cuentaRegresiva(n)
